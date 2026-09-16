0b78a1e2b4409790e69dd8649ee9d98b2488602e
Simar Malhotra <malhotrasimar009@gmail.com>
2026-09-16 03:10:21 -0400

    beautify + known bugs
    Signed-off-by: Simar Malhotra <malhotrasimar009@gmail.com>

--- greo/src/main.rs
+++ greo/src/main.rs
@@ -1,10 +1,39 @@
-use std::process::Command;
+use core::fmt;
+use std::{fmt::Display, process::Command};
 
-#[derive(Debug)]
+/// Known bug: This way of impl probably treats the '10' in -m 10 when
+/// passed a cmd line arg as a positional arg;
+/// We should maybe start parsing from the right, take last as search path
+/// second last as pattern, and everything else as std args.
+///
 struct SearchResult<'a> {
     file_path: &'a str,
     line_num: u32,
-    contents: &'a str,
+    contents_before_match: &'a str,
+    contents_match: &'a str,
+    contents_after_match: &'a str,
+}
+
+impl<'a> fmt::Display for SearchResult<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let indent_width = self
+            .contents_before_match
+            .chars()
+            .take_while(|c| c.is_whitespace())
+            .map(|c| if c == '\t' { 2 } else { 1 })
+            .sum::<usize>();
+        let contents_before_match_s = self.contents_before_match.trim_start_matches([' ', '\t']);
+        write!(
+            f,
+            "{}:\x1b[93m{}\x1b[0m:{}{}\x1b[91m{}\x1b[0m{}\n",
+            self.file_path,
+            self.line_num,
+            "\u{2218}".repeat(indent_width),
+            contents_before_match_s,
+            self.contents_match,
+            self.contents_after_match,
+        )
+    }
 }
 
 struct InputArgs {
@@ -51,7 +80,7 @@ fn split_result(output: &str) -> Vec<&str> {
     output.lines().collect()
 }
 
-fn parse_result<'a>(split_output: &[&'a str]) -> Vec<SearchResult<'a>> {
+fn parse_result<'a>(pattern: &'a str, split_output: &[&'a str]) -> Vec<SearchResult<'a>> {
     let mut res = Vec::new();
 
     for so in split_output {
@@ -67,10 +96,17 @@ fn parse_result<'a>(split_output: &[&'a str]) -> Vec<SearchResult<'a>> {
             continue;
         };
 
+        let Some((contents_before_match, contents_after_match)) = contents.split_once(pattern)
+        else {
+            continue;
+        };
+
         res.push(SearchResult {
             file_path,
             line_num,
-            contents,
+            contents_before_match,
+            contents_match: pattern,
+            contents_after_match,
         });
     }
 
@@ -96,9 +132,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let stdout = String::from_utf8_lossy(&output.stdout);
     let split_output = split_result(&stdout);
-    let search_results: Vec<SearchResult> = parse_result(split_output.as_slice());
+    let search_results: Vec<SearchResult> = parse_result(pattern, split_output.as_slice());
     for sr in search_results {
-        println!("{:?}", sr);
+        println!("{}", sr);
     }
 
     Ok(())
