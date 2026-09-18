6f460ca74a99beb7b2ac65046f9db88810e580a9
Simar Malhotra <malhotrasimar009@gmail.com>
2026-09-17 21:43:02 -0400

    I actually just saw this shi in emacs where it doesn't the output to be produced by grep or nun like that; It just parse any output for a filepath, with or without a line number, and just let's the user open it on click. I might just impl that since it general purpose, and much simpler tbf
    Signed-off-by: Simar Malhotra <malhotrasimar009@gmail.com>

--- greo/src/main.rs
+++ greo/src/main.rs
@@ -1,78 +1,25 @@
 use core::fmt;
-use std::{fmt::Display, process::Command};
+use std::process::Command;
 
 /// Known bug: This way of impl probably treats the '10' in -m 10 when
 /// passed a cmd line arg as a positional arg;
 /// We should maybe start parsing from the right, take last as search path
 /// second last as pattern, and everything else as std args.
 ///
+
 struct SearchResult<'a> {
     file_path: &'a str,
     line_num: u32,
-    contents_before_match: &'a str,
-    contents_match: &'a str,
-    contents_after_match: &'a str,
+    contents: &'a str,
 }
 
 impl<'a> fmt::Display for SearchResult<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let indent_width = self
-            .contents_before_match
-            .chars()
-            .take_while(|c| c.is_whitespace())
-            .map(|c| if c == '\t' { 2 } else { 1 })
-            .sum::<usize>();
-        let contents_before_match_s = self.contents_before_match.trim_start_matches([' ', '\t']);
         write!(
             f,
-            "{}:\x1b[93m{}\x1b[0m:{}{}\x1b[91m{}\x1b[0m{}\n",
-            self.file_path,
-            self.line_num,
-            "\u{2218}".repeat(indent_width),
-            contents_before_match_s,
-            self.contents_match,
-            self.contents_after_match,
+            "{}:{}:{}\n",
+            self.file_path, self.line_num, self.contents,
         )
-    }
-}
-
-struct InputArgs {
-    args: Vec<String>,
-}
-
-impl Default for InputArgs {
-    fn default() -> Self {
-        Self { args: Vec::new() }
-    }
-}
-
-struct GrepInputArgs<'a> {
-    path: &'a str,
-    pattern: &'a str,
-    named_args: Vec<&'a str>,
-}
-
-impl<'a> From<&'a InputArgs> for GrepInputArgs<'a> {
-    fn from(args: &'a InputArgs) -> Self {
-        Self {
-            path: &args.args[1],
-            pattern: &args.args[2],
-            named_args: vec!["-rni", "-m", "10"],
-        }
-    }
-}
-impl<'a> Default for GrepInputArgs<'a> {
-    fn default() -> Self {
-        Self {
-            path: "/Users/0saker/Desktop/code/probe/fdrs/src/",
-            pattern: "run",
-            named_args: vec!["-rni", "-m", "10"],
-        }
-    }
-}
-impl<'a> GrepInputArgs<'a> {
-    fn into_parts(self) -> (&'a str, &'a str, Vec<&'a str>) {
-        (self.path, self.pattern, self.named_args)
     }
 }
 
@@ -80,7 +27,7 @@ fn split_result(output: &str) -> Vec<&str> {
     output.lines().collect()
 }
 
-fn parse_result<'a>(pattern: &'a str, split_output: &[&'a str]) -> Vec<SearchResult<'a>> {
+fn parse_result<'a>(split_output: &[&'a str]) -> Vec<SearchResult<'a>> {
     let mut res = Vec::new();
 
     for so in split_output {
@@ -96,17 +43,10 @@ fn parse_result<'a>(pattern: &'a str, split_output: &[&'a str]) -> Vec<SearchRes
             continue;
         };
 
-        let Some((contents_before_match, contents_after_match)) = contents.split_once(pattern)
-        else {
-            continue;
-        };
-
         res.push(SearchResult {
             file_path,
             line_num,
-            contents_before_match,
-            contents_match: pattern,
-            contents_after_match,
+            contents,
         });
     }
 
@@ -115,24 +55,19 @@ fn parse_result<'a>(pattern: &'a str, split_output: &[&'a str]) -> Vec<SearchRes
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
+    let command = &args[1];
+    let command_args = &args[2..];
 
-    let input = InputArgs { args };
+    let mut cmd = Command::new(command);
+    cmd.args(command_args);
 
-    let (path, pattern, named_args) = if input.args.len() < 3 {
-        GrepInputArgs::default().into_parts()
-    } else {
-        GrepInputArgs::from(&input).into_parts()
-    };
+    println!("Command: {:?}", cmd);
 
-    let output = Command::new("grep")
-        .args(named_args)
-        .arg(pattern)
-        .arg(path)
-        .output()?;
+    let output = cmd.output()?;
 
     let stdout = String::from_utf8_lossy(&output.stdout);
     let split_output = split_result(&stdout);
-    let search_results: Vec<SearchResult> = parse_result(pattern, split_output.as_slice());
+    let search_results: Vec<SearchResult> = parse_result(split_output.as_slice());
     for sr in search_results {
         println!("{}", sr);
     }
